43a22b2e712c1586d1ceb113f6a4b9db1e1b87b8
wisdomtt <curtismichaelmason@gmail.com>
2017-03-21 18:15:35 -0400

    Patch

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -65,8 +65,8 @@ public:
 		// Get a CvSink. This will capture Mats from the Camera
 		cs::CvSink cvSink = CameraServer::GetInstance()->GetVideo();
 		// Setup a CvSource. This will send images back to the Dashboard
-		cs::CvSource outputStream = CameraServer::GetInstance()->
-		PutVideo("Rectangle", 640, 480);
+//		cs::CvSource outputStream = CameraServer::GetInstance()->
+//		PutVideo("Rectangle", 640, 480);
 		// Mats are very memory expensive. Lets reuse this Mat.
 		cv::Mat mat;
 		cvSink.GrabFrame(mat);
